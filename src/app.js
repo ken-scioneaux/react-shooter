497d6e78fb0cf3999096ae78729593a2a1a83e20
Daniel Bruce <dbruce1126@gmail.com>
2016-09-19 22:25:43 -0400

    Add redux-logger.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
 import reducer from './Game/Reducers';
 import Game from './Game';
 
+const INIT_STORE_ACTION = 'init';
 const ROOT_ID = 'game-content';
 const reactRoot = document.getElementById(ROOT_ID);
 if (reactRoot) {
+  const store = createStore(reducer, applyMiddleware(createLogger()));
+  store.dispatch({ type: INIT_STORE_ACTION });
   render(
-    <Game store={ createStore(reducer) } />,
+    <Game store={ store } />,
     reactRoot
   );
 } else {
