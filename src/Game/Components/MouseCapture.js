c83769d20ed9bd07e563b21d69ac8318a7e74cbd
Andrew Ballantyne <ballantyno@gmail.com>
2016-09-23 00:30:29 -0400

    Fixes the greatest travesty of our time.

--- src/Game/Components/MouseCapture.js
+++ src/Game/Components/MouseCapture.js
@@ -2,14 +2,13 @@ import React, { Component, PropTypes } from 'react';
 import connect from '../../Lib/connect';
 import { moveShip } from '../Actions/ship';
 
-class MouseCapture extends Component
-{
+class MouseCapture extends Component {
+  static propTypes = {
+    onMoveShip: PropTypes.func.isRequired,
+  };
   constructor(props) {
     super(props);
     this.onMouseMove = this.onMouseMove.bind(this);
-  }
-  static propTypes = {
-    onMoveShip: PropTypes.func.isRequired,
   }
   onMouseMove(e) {
     // calculate the position of the mouse pointer relative to the stage
